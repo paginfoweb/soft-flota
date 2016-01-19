1b9fb0fcce187f3507d1d94e9f1976929dfc101b
Angel Fuenmayor <paginfoweb@gmail.com>
2016-01-19 00:58:31 -0430

    Se subieron nuevas traducciones

---
+++ resources/lang/en/app.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'section_title_workshops' => 'Workshops',
+    'create_workshop_title' => 'Create Workshop',
+    'workshop_title' => 'Workshop',
+    'status_title' => 'Status',
+    'phone_title' => 'Phone',
+    'action_title' => 'Action',
+    'title_name' => 'Name',
+    'complete_name' => 'Complete Name',
+    'manager_name' => 'Manage Name',
+    'telephone_contact' => 'Telephone Contact',
+    'title_address' => 'Address',
+    'contact_title' => 'Contact',
+    'search_workshop' => 'Search Workshop',
+    'search_not_found_message' => 'Sorry no records that match your search',
+    'title_record' => 'Record',
+    'title_cancel' => 'Cancel',
+    'title_update' => 'Update',
+    'created_title' => 'Created',
+    'last_update' => 'Last update of data',
+    'title_registered' => 'Registered',
+    'email' => 'Email',
+    'type' => 'Type',
+    'user_detail' => 'User detail',
+    'workshop_detail' => 'Workshop detail',
+    'detail_store' => 'Detail store',
+    'store_associated' => 'Store associated',
+    'associated_workshop' => 'Associated workshop',
+    'edit_workshop' => 'Edit workshop',
+    'edit_store' => 'Edit store',
+    'list_workshops' => 'List workshops',
+    'list_stores' => 'List stores',
+    'users_list' => 'Users list',
+    'stores' => 'Stores',
+    'store' => 'Store',
+    'create_store' => 'Create store',
+    'users' => 'Users',
+    'create_user' => 'Create user',
+    'find_store' => 'Find store',
+
+];
---
+++ resources/lang/es/app.php
@@ -0,0 +1,58 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'section_title_workshops' => 'Talleres',
+    'create_workshop_title' => 'Crear Taller',
+    'workshop_title' => 'Taller',
+    'status_title' => 'Estatus',
+    'phone_title' => 'Teléfono',
+    'action_title' => 'Acción',
+    'title_name' => 'Nombre',
+    'complete_name' => 'Nombre Completo',
+    'manager_name' => 'Nombre del Encargado',
+    'telephone_contact' => 'Teléfono de Contacto',
+    'title_address' => 'Dirección',
+    'contact_title' => 'Contácto',
+    'search_workshop' => 'Buscar Taller',
+    'search_not_found_message' => 'Lo sentimos no hay registros que coincidan con su busqueda',
+    'title_record' => 'Registrar',
+    'title_cancel' => 'Cancelar',
+    'title_update' => 'Actualizar',
+    'created_title' => 'Creado',
+    'last_update' => 'Última actualización de datos',
+    'title_registered' => 'Registrado',
+    'email' => 'Email',
+    'type' => 'Tipo',
+    'user_detail' => 'Detalle Usuario',
+    'workshop_detail' => 'Detalle Ealler',
+    'detail_store' => 'Detalle Almacen',
+    'store_associated' => 'Almacen asociado',
+    'associated_workshop' => 'Taller asociado',
+    'edit_workshop' => 'Editar taller',
+    'edit_store' => 'Editar almacen',
+    'list_workshops' => 'Listar talleres',
+    'list_stores' => 'Listar almacenes',
+    'users_list' => 'Listar usuarios',
+    'stores' => 'Almacenes',
+    'store' => 'Almacén',
+    'create_store' => 'Crear Almacén',
+    'users' => 'Usuarios',
+    'create_user' => 'Crear usuario',
+    'find_store' => 'Buscar almacén',
+
+
+
+
+];
